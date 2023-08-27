aa95a342a394576a759d0a7cf91070f1133fc6d1
Елена <lenna@gmail.com>
2023-08-27 16:20:54 +0300

    Added program in Task58

--- Task58/Program.cs
+++ Task58/Program.cs
@@ -36,15 +36,44 @@ void PrintMatrix(int[,] matrix)
     }
 
 }
-
-int[,] MultiplicatiomTwoMatrix(int[,] matrix_A, int[,] matrix_B){
-
-    int[,] matrix_multi = new int[matrix_A.GetLength(0),matrix_B.GetLength(1)];
-
-     for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            for (int k = 0; k < matrix.GetLength(1) - 1; k++)
-            {
+bool ChangeMatrix(int[,] matrix_A, int[,] matrix_B)
+{
+    return matrix_A.GetLength(0) == matrix_B.GetLength(1);
 }
+
+int[,] MultiplicatiomTwoMatrix(int[,] matrix_A, int[,] matrix_B)
+{
+
+    int[,] matrix_multi = new int[matrix_A.GetLength(0), matrix_B.GetLength(1)];
+
+    for (int i = 0; i < matrix_multi.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix_multi.GetLength(1); j++)
+        {
+            int summ = 0;
+            for (int k = 0; k < matrix_A.GetLength(1); k++)
+            {
+
+                summ = summ + matrix_A[i, k] * matrix_B[k, j];
+            }
+            matrix_multi[i, j] = summ;
+        }
+
+    }
+    return matrix_multi;
+}
+
+int[,] array_2d_A = CreateMartixRndInt(2, 2, 1, 3);
+
+int[,] array_2d_B = CreateMartixRndInt(2, 2, 1, 3);
+
+if (!ChangeMatrix(array_2d_A,array_2d_B)) {
+Console.WriteLine("Невозможно перемножить матрицы!");
+return;}
+PrintMatrix(array_2d_B);
+Console.WriteLine();
+PrintMatrix(array_2d_A);
+Console.WriteLine();
+int[,] multi_matrix = MultiplicatiomTwoMatrix(array_2d_A, array_2d_B);
+PrintMatrix(multi_matrix);
+
