888e05c15b65bffe351f994cea6958ece424f3da
Елена <lenna@gmail.com>
2023-08-27 15:29:33 +0300

    Added program in Task56

--- Task56/Program.cs
+++ Task56/Program.cs
@@ -1,2 +1,87 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] CreateMartixRndInt(int rows, int colums, int min, int max)
+{
+    int[,] matrix = new int[rows, colums];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],+5}");
+
+        }
+        Console.WriteLine("| ");
+    }
+
+}
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+
+    Console.WriteLine("]");
+}
+
+int[] SumElemRowMatrix(int[,] matrix)
+{
+    int[] sum = new int[matrix.GetLength(0)];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum[i] = matrix[i, j] + sum[i];
+        }
+    }
+
+    return sum;
+}
+void PrintRowMinSum(int[] array)
+{
+    int min_sum = array[0];
+    int num = 1;
+    for (int i = 1; i < array.Length; i++)
+    {  if (array[i] <= min_sum)
+        {
+            min_sum = array[i];
+            num = i + 1;
+
+        }
+
+    }
+    Console.WriteLine($"Номер строки с наименьшей суммой элементов = {num}");
+}
+
+int[,] array_2d = CreateMartixRndInt(4, 3, 1, 30);
+PrintMatrix(array_2d);
+Console.WriteLine();
+int[] arr = SumElemRowMatrix(array_2d);
+PrintArray(arr);
+PrintRowMinSum(arr);
